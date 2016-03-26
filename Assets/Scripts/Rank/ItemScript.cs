03635a39a770c10bafc8aa074edbf196253505c0
high4123 <high4123@gmail.com>
2016-03-26 15:04:28 +0900

    랭크 뷰 씬 추가
    리스트 뷰 씬 추가

---
+++ Assets/Scripts/Rank/ItemScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour {
+
+	public UILabel Label1;
+	public UILabel Label2;
+	public UILabel Label3;
+	public UILabel Label4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Rank/RankItemScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RankItemScript : MonoBehaviour {
+
+	public GameObject RankItem;
+	public GameObject GridRank;
+	public GameObject Prefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int vI = 0; vI < 10; vI++) {
+			Prefab = NGUITools.AddChild (GridRank, RankItem);
+			Prefab.GetComponent<ItemScript> ().Label1.text = vI + "Test!";
+		}
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
