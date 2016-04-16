394620ac04ce341bea9383b776ad9348d0260fcc
high4123 <high4123@gmail.com>
2016-04-16 19:08:15 +0900

    Revert "서버 연결 테스트1"
    This reverts commit 6910c8acc24fe4e33333d47d031d9d51a59116b1.

--- Assets/Scripts/Rank/ItemScript.cs
+++ Assets/Scripts/Rank/ItemScript.cs
@@ -1,9 +1,5 @@
-﻿using UnityEngine; 
-using System.Collections; 
-using System.Collections.Generic; 
-using System.Text; 
-using System.IO; 
-using System.Net;
+﻿using UnityEngine;
+using System.Collections;
 
 public class ItemScript : MonoBehaviour {
 
@@ -19,59 +15,6 @@ public class ItemScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Dictionary<string, string> PostDic = new Dictionary <string, string> {
-			{ "userid", "test" },
-			{ "score", "2000" }
-		};
-
-		POST("http://ikunew.iptime.org:8000/new", PostDic);
-	}
-
-	public void GET(string url) 
-	{ 
-		WWW www = new WWW(url); 
-
-		StartCoroutine(WaitForRequest(www));
-	} 
-
-	public void POST(string url, Dictionary<string, string> post) 
-	{ 
-		WWWForm form = new WWWForm(); 
-
-		foreach (KeyValuePair<string, string> post_arg in post) 
-		{ 
-			form.AddField(post_arg.Key, post_arg.Value); 
-		} 
-
-		WWW www = new WWW(url, form); 
-
-		StartCoroutine(WaitForRequest(www));
-	} 
-
-
-	private IEnumerator WaitForRequest(WWW www) 
-	{ 
-		yield return www; 
-
-		// check for errors 
-		if (www.error == null) 
-		{ 
-			Debug.Log("WWW Ok!: " + www.text); 
-		} 
-		else 
-		{ 
-			Debug.Log("WWW Error: " + www.error); 
-		} 
-	}
-
-	void OnMouseDown(){
-		Debug.Log("Click!");
-		/*
-		 * Dictionary<string, string> PostDic = new Dictionary <string, string> {
-			{ "userid", "test" },
-			{ "score", "2000" }
-		};
-
-		POST("http://ikunew.iptime.org:8000/new", PostDic);		*/
+	
 	}
 }
