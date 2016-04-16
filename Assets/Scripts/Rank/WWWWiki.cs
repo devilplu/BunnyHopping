6910c8acc24fe4e33333d47d031d9d51a59116b1
high4123 <high4123@gmail.com>
2016-04-16 17:04:08 +0900

    서버 연결 테스트1
    연결테스트

---
+++ Assets/Scripts/Rank/WWWWiki.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine; 
+using System.Collections; 
+using System.Collections.Generic; 
+using System.Text; 
+using System.IO; 
+using System.Net;
+
+public class WWWWiki : MonoBehaviour 
+{ 
+	// Use this for initialization 
+	void Start() 
+	{          
+	} 
+
+	// Update is called once per frame 
+	void Update() 
+	{ 
+
+	} 
+
+	void OnGUI() 
+	{
+
+	} 
+
+	public WWW GET(string url) 
+	{ 
+		WWW www = new WWW(url); 
+		StartCoroutine(WaitForRequest(www)); 
+		return www; 
+	} 
+
+	public WWW POST(string url, Dictionary<string, string> post) 
+	{ 
+		WWWForm form = new WWWForm(); 
+
+		foreach (KeyValuePair<string, string> post_arg in post) 
+		{ 
+			form.AddField(post_arg.Key, post_arg.Value); 
+		} 
+
+		WWW www = new WWW(url, form); 
+		StartCoroutine(WaitForRequest(www)); 
+		return www; 
+	} 
+
+
+	private IEnumerator WaitForRequest(WWW www) 
+	{ 
+		yield return www; 
+		
+			// check for errors 
+		if (www.error == null) 
+		{ 
+			Debug.Log("WWW Ok!: " + www.text); 
+		} 
+		else 
+		{ 
+			Debug.Log("WWW Error: " + www.error); 
+		} 
+	}
+}
